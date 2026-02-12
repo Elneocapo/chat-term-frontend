c6f0248ff111f73cf246c2bef514fcb2c06e4a92
Neo Ferrer Mohedano <118294424+Elneocapo@users.noreply.github.com>
2026-02-12 02:36:29 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,91 @@
+//El navegador intenta conectarse al servidor. Es como marcar el número de teléfono.
+const ws = new WebSocket("wss://chat-term-servidor.onrender.com");
+//supongo que sustituyes localhost3000 por el url del servidor
+const rejiondemensajes = document.querySelector('#mensajeria');
+
+const nombre_usuario = localStorage.getItem('usuarioGuardado');
+
+const arraydecosas = [
+  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+];
+
+//Aquí el cliente escucha mensajes que manda el servidor.
+ws.onmessage = (event) => {
+    event.data = event.data.toString();
+    let data_del_server = event.data.split(","); 
+    console.log(data_del_server)
+
+    if(data_del_server.length === 1){
+        RecivirmensajeSystem(data_del_server);
+    }else{
+        Recivirmensaje(data_del_server)
+    }
+
+        const parrafo = document.getElementById('mensajeria');
+
+        // 2. Comprobamos si el contenido es más alto que el contenedor
+        if (parrafo.scrollHeight > parrafo.clientHeight) {
+            
+            // 3. Limpiamos el contenido
+            parrafo.innerHTML = ""; 
+            
+            if(data_del_server.length === 1){
+                RecivirmensajeSystem(data_del_server);
+            }else{
+                Recivirmensaje(data_del_server)
+            }
+        }
+};
+function Recivirmensaje(mensaje){
+    rejiondemensajes.innerHTML += `${mensaje[0]} > ${mensaje[1]}<br> `;
+}
+
+function RecivirmensajeSystem(mensaje){
+    rejiondemensajes.innerHTML += `---------- > ${mensaje}<br>`;
+
+}
+
+let idjugador = ""
+
+for(let i = 0; i < 10; i++){
+    idjugador = idjugador + arraydecosas[Math.floor(Math.random() * arraydecosas.length)];
+}
+
+
+//ejecuta al abrir la conexion
+ws.onopen = () => {
+    ws.send(`cliente conectado id : ${idjugador}`);
+    console.log(`Conectado. tu id es ${idjugador}`);
+
+    ///////////////////////////////////
+    const boton = document.querySelector('.sendbutton');
+    const input = document.querySelector('#userInput');
+
+
+    input.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            const texto = input.value;
+            if(texto.trim() !== ""){
+                //ws.send(idjugador + "ALLCLIENTS_LOG" + texto);
+                ws.send([nombre_usuario, "ALLCLIENTS_LOG" + texto]);
+                input.value = "";
+            }
+        }
+    });
+
+    boton.addEventListener('click', ()=>{
+        const texto = input.value;
+        if(texto.trim() !== ""){
+            //ws.send(idjugador + "ALLCLIENTS_LOG" + texto);
+            ws.send([nombre_usuario, "ALLCLIENTS_LOG" + texto]);
+            input.value = "";
+        }
+    });
+
+
+};
+
+
+
---
+++ seleccionname.js
@@ -0,0 +1,27 @@
+//El navegador intenta conectarse al servidor. Es como marcar el número de teléfono.
+const ws = new WebSocket("wss://chat-term-servidor.onrender.com");
+//supongo que sustituyes localhost3000 por el url del servidor
+
+//Aquí el cliente escucha mensajes que manda el servidor.
+ws.onmessage = (event) => {
+    
+};
+
+
+//ejecuta al abrir la conexion
+ws.onopen = () => {
+  const inputname = document.querySelector(`#userInputnombre`);
+  const botonname = document.querySelector(`.sendbuttonnombre`);
+
+    botonname.addEventListener('click', ()=>{
+        const texto = inputname.value;
+        if(texto.trim() !== ""){
+            //ws.send(idjugador + "ALLCLIENTS_LOG" + texto);
+            localStorage.setItem('usuarioGuardado', inputname.value);
+            window.location.href = "chat.html";
+        }
+    });
+};
+
+
+
